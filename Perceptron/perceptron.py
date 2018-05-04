b57d035ce433d75ac42d68b1d1ca985c7b780453
Your Name <you@example.com>
2018-05-04 08:27:17 -0300

    Finalizado git add .!

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,10 +1,12 @@
 import numpy as np
+from sklearn.utils import shuffle
+
 
 class Perceptron(object):
     """Implements a perceptron network"""
-    def __init__(self, input_size, saida, epochs=1, alpha=0.1):
+    def __init__(self, input_size, saida, epochs=10000, alpha=0.001):
         # print("init")
-        self.W = np.zeros(shape=(saida,input_size))
+        self.W = np.random.rand(saida,input_size)
         self.b = np.zeros(shape=(saida,1))
         self.alpha = alpha
         self.erro = 0
@@ -39,14 +41,6 @@ class Perceptron(object):
         a = self.step(z)
         return a
 
-    # def transpose1(self,x):
-    #     #print(x.shape)
-    #     y = np.zeros(shape=(13,x.shape[1]))
-    #     y = x.T
-    #     print(str(x.shape)+"njsdfnk")
-    #     print(y)
-    #     print(x)
-    #     return y
 
     def perceptron(self, X, d):
         #aux = np.zeros(shape=(1,13))
@@ -54,7 +48,7 @@ class Perceptron(object):
         # print("fit")
         t =0
         E = 1
-        while t < self.epochs:
+        while t < self.epochs and E>0:
             E = 0
             for i in range(X.shape[0]):
                 x = X[i]
@@ -63,25 +57,22 @@ class Perceptron(object):
                 e = d[i].reshape(3,1) - y
                 aux = x.reshape(1,13)
                 e = e.reshape(3,1)
-                self.W = self.W + self.alpha*e * aux
+                self.W = self.W + self.alpha* np.matmul(e,aux)
                 self.b = self.b + self.alpha*e
-                E = E + e * e
-                #print(e.shape)
-                self.erro = E
+                E = E + np.sum([value*value for value in e])
+            print("Erro:")
+            print(E)
+                # self.erro = E
             t= t+1
 
+        print(E)
+
     def accuracy(self,actual,predicted):
+        print(actual)
+        print(predicted)
         teste = 0
-        for i in range(len(actual)):
-            # print("----")
-            # print(actual[i])
-            # print(predicted[i])
-            # print("------")
-            if actual[i] == predicted[i]:
-                teste+=1
-            if(teste ==3):
-                print("jbsdf")
-                self.total+=1
+        if(actual[0]==predicted[0] and actual[1]==predicted[1] and actual[2]==predicted[2]):
+            self.total +=1
 
 
 def manipulaArquivo():
@@ -111,22 +102,33 @@ def manipulaArquivo():
 def main():
 
     X, d = manipulaArquivo()
-    # X = np.array([
-    #     [1,2,3,4,5,6,7,8,9,10,11,12,13],
-    #     [1,2,3,4,5,6,7,8,9,10,11,12,13],
-    #     [1,2,3,4,5,6,7,8,9,10,11,12,13],
-    #     [1,5,65,53,123,3,312,321,231,231,123,123,3]
-    # ])
-    # d = np.array([[0,0,1],[0,1,0],[0,1,0],[0,0,1]])
-    # print(d[0].shape)
-    # print(d[0].transpose().shape)
-    print(X.shape)
-    print(d.shape)
 
-    perceptron = Perceptron(input_size=13, saida=3,epochs=1)
-    perceptron.perceptron(X, d)
+
+    aux = np.mean(X,axis=0)
+    #print(aux)
+    aux2 = np.std(X, axis=0)
+    #print(aux2)
+    X = (X-aux)/aux2
+    # print(X)
+
+
+    treino = X[:115]
+    d_treino = d[:115]
+    #print(treino.shape)
+    teste = X[116:178]
+    d_teste = d[116:178]
+
+    perceptron = Perceptron(input_size=13, saida=3,epochs=10000)
+    perceptron.perceptron(treino, d_treino)
+
+    for i in range(0,teste.shape[0]):
+        # print(perceptron.predict(X[i]))
+        # print(d[i])
+        perceptron.accuracy(perceptron.predict(teste[i]),d_teste[i])
+    print(perceptron.total/teste.shape[0])
+
     print(perceptron.W)
-    print(perceptron.erro)
-    print(perceptron.total/178)
+    #print(perceptron.erro)
+
 
 main()
