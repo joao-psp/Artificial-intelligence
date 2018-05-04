0a8400d2e8c086116a918cd2f4c825a22880a7e8
João <joao.psp@hotmail.com>
2018-05-04 17:02:19 -0300

    Enviado

---
+++ Perceptron/README.md
@@ -0,0 +1,5 @@
+Para executar basta rodar o comando:
+
+python3 perceptron.py
+
+A saída é: a matriz W o vetor Bias, o erro E e a accuracy encontrada.
--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,6 +1,4 @@
 import numpy as np
-from sklearn.utils import shuffle
-
 
 class Perceptron(object):
     """Implements a perceptron network"""
@@ -11,13 +9,10 @@ class Perceptron(object):
         self.alpha = alpha
         self.erro = 0
         self.total = 0
-        # print(self.b.shape)
-        # print(self.W)
-        # print(self.W.shape)
         self.epochs = epochs
         # print('\n')
 
-    def step(self, x):
+    def step(self, x):   # aplica a funcao degrau
         # print("activation_fn")
         # print(x.shape)
         y = np.zeros(shape=(x.shape))
@@ -26,26 +21,16 @@ class Perceptron(object):
                 y[i] = 1
             else:
                 y[i] = 0
-        # print("y: ")
-        # print(y)
-        # print('\n')
         return y
 
-    def predict(self, x):
-        # print('\n')
-        # print("Predict")
-        # print(x.shape)
-        # print(self.W.shape)
+    def predict(self, x): # f(Wx+b)
         z = np.matmul(self.W,x).reshape(3,1) + self.b
-        # print(z)
         a = self.step(z)
         return a
 
 
-    def perceptron(self, X, d):
-        #aux = np.zeros(shape=(1,13))
-        # print('\n')
-        # print("fit")
+    def perceptron(self, X, d): # algortmo para treino
+
         t =0
         E = 1
         while t < self.epochs and E>0:
@@ -60,16 +45,11 @@ class Perceptron(object):
                 self.W = self.W + self.alpha* np.matmul(e,aux)
                 self.b = self.b + self.alpha*e
                 E = E + np.sum([value*value for value in e])
-            print("Erro:")
-            print(E)
-                # self.erro = E
+            self.erro = E
             t= t+1
 
-        print(E)
+    def accuracy(self,actual,predicted):  # verifica se a saida e igual a desejada
 
-    def accuracy(self,actual,predicted):
-        print(actual)
-        print(predicted)
         teste = 0
         if(actual[0]==predicted[0] and actual[1]==predicted[1] and actual[2]==predicted[2]):
             self.total +=1
@@ -96,7 +76,6 @@ def manipulaArquivo():
             saidas[i] = [0,1,0]
         elif saidas[i] ==3:
             saidas[i] = [0,0,1]
-    #print(entradas)
     return np.array(entradas), np.array(saidas)
 
 def main():
@@ -104,31 +83,37 @@ def main():
     X, d = manipulaArquivo()
 
 
-    aux = np.mean(X,axis=0)
-    #print(aux)
+    aux = np.mean(X,axis=0)   # normaliza arquivo
     aux2 = np.std(X, axis=0)
-    #print(aux2)
     X = (X-aux)/aux2
-    # print(X)
 
 
-    treino = X[:115]
+    treino = X[:115]    #separa o banco em teste e treino
     d_treino = d[:115]
-    #print(treino.shape)
+
     teste = X[116:178]
     d_teste = d[116:178]
 
-    perceptron = Perceptron(input_size=13, saida=3,epochs=10000)
+    perceptron = Perceptron(input_size=13, saida=3,epochs=10000) #treina a base
     perceptron.perceptron(treino, d_treino)
 
     for i in range(0,teste.shape[0]):
-        # print(perceptron.predict(X[i]))
-        # print(d[i])
-        perceptron.accuracy(perceptron.predict(teste[i]),d_teste[i])
-    print(perceptron.total/teste.shape[0])
+        perceptron.accuracy(perceptron.predict(teste[i]),d_teste[i]) # testa a base
 
+    print("Alpha = 0.001, max_epochs = 10000")
+
+    print("\n Matrix W ")
     print(perceptron.W)
-    #print(perceptron.erro)
+    print("\n bias")
+    print(perceptron.b)
+
+    print("\nAccuracy: ")
+    print(perceptron.total/teste.shape[0]) # mostra a accuracy
+
+    print("\nErro E")
+    print(perceptron.erro)
+
+
 
 
 main()
